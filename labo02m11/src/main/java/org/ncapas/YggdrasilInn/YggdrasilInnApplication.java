c42d529dc3c64f308137c0dd8f773cb3e4506d16
kattia-ss <00036122@uca.edu.sv>
2025-05-06 22:43:28 -0600

    add: proyecto base

---
+++ labo02m11/src/main/java/org/ncapas/YggdrasilInn/YggdrasilInnApplication.java
@@ -0,0 +1,13 @@
+package org.ncapas.YggdrasilInn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YggdrasilInnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YggdrasilInnApplication.class, args);
+    }
+
+}
---
+++ labo02m11/src/test/java/com/example/labo02m11/YggdrasilInnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labo02m11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YggdrasilInnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
